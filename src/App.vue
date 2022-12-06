ab4cc5adab90639ee4a1e413a1cd869147191c97
Mario Rogic <hello@mario.net.au>
2022-12-06 16:49:48 +0000

    Add vue-final-modal

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import { ModalsContainer } from 'vue-final-modal'
 </script>
 
 <template>
@@ -18,6 +19,7 @@ import HelloWorld from './components/HelloWorld.vue'
   </header>
 
   <RouterView />
+  <ModalsContainer />
 </template>
 
 <style scoped>
--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import { createVfm } from 'vue-final-modal';
+
 import './assets/main.css'
 
 const app = createApp(App)
 
 app.use(router)
+
+const vfm = createVfm();
+app.use(vfm);
 
 app.mount('#app')
